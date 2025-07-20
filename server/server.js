4a737d36a3241e3c23f6e657c5228688855af8ee
uvais-malik <uvaismalik2621@gmail.com>
2025-07-20 17:37:58 +0530

    fix: Resolve Vercel cold start issue

--- server/server.js
+++ server/server.js
@@ -8,30 +8,22 @@ import blogRouter from './routes/blogRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const startServer = async () => {
-  try {
-    await connectDB();
+await connectDB();
 
-    // Middleware
-    app.use(cors());
-    app.use(express.json());
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-    // Routes
-    app.get('/', (req, res) => {
-      res.send("API is working");
-    });
-    app.use('/api/admin', adminRouter);
-    app.use('/api/blog', blogRouter);
+// Routes
+app.get('/', (req, res) => {
+  res.send("API is working");
+});
+app.use('/api/admin', adminRouter);
+app.use('/api/blog', blogRouter);
 
-    app.listen(PORT, () => {
-      console.log('Server is running on port ' + PORT);
-    });
-  } catch (error) {
-    console.error("Failed to start server:", error);
-  }
-};
-
-startServer();
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
+});
 
 export default app;
 
